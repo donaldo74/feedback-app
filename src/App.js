0a1705edcd4b1c4005e21e9fec249bddac7412bc
Gavin Coleman <gavinjcoleman@gmail.com>
2022-10-10 21:57:55 +0100

    Conditionals

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-function App () {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 2, text: 'Comment Two'},
-    {id: 3, text: 'Comment Three'},
-  ]
-
+function App () { 
   return (
-    <>
-      <h1>{title.toUpperCase()}</h1>
-     <p>{body}</p>
-     <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) =>(
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-     </div>
-    </>
-    
+    <div className="container">
+      <h1>My App</h1>
+    </div>
   ) 
 }
 
