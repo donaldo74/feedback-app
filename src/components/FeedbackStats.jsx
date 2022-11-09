d81e91b6d62148b7a57b423ff2559f051e78fcee
Gavin Coleman <gavinjcoleman@gmail.com>
2022-11-09 10:11:16 +0000

    Stats working fine

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,23 @@
+import propTypes from 'prop-types'
+
+function FeedbackStats({ feedback }) {
+  // Calculate ratings average
+  let average = feedback.reduce((acc, cur) => {
+    return acc + cur.rating
+  }, 0) / feedback.length
+  
+  //Show average to 1 decimal place and use regex to clip off any trailing 0's
+  average=average.toFixed(1).replace(/[.,]0$/, '')
+
+  return (
+    <div className='feedback-stat'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  )
+}
+
+FeedbackStats.propTypes = {
+  feedback: propTypes.array.isRequired,
+}
+export default FeedbackStats
