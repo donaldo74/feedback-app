5298781623c529ed8a92434f86e84c9794df20cc
Gavin Coleman <gavinjcoleman@gmail.com>
2022-11-11 10:32:59 +0000

    small change

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -22,17 +22,19 @@ function FeedbackForm() {
     }
     setText(e.target.value)
   }
-  return <Card>
-    <form >
-      <h2>How would you rate your service with us?</h2>
-      <RatingSelect select={(rating) => setRating(rating)} />
-      <div className="input-group">
-        <input onChange={handleTextChange} type="text" placeholder="Write a review" value={ text} />
-        <Button type="submit" isDisabled={btnDisabled}>Send</Button>
-      </div>
-      {message && <div className='message'>{message}</div>}
-    </form>
-  </Card>  
+  return (
+    <Card>
+      <form >
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={ text} />
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+  </Card>
+  )
 }
 
 export default FeedbackForm
