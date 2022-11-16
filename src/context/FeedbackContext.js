9f3837fe7334fb3335ebffdf434d132316262b40
Gavin Coleman <gavinjcoleman@gmail.com>
2022-11-16 19:12:46 +0000

    All updating ok

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -40,6 +40,11 @@ export const FeedbackProvider = ({ children }) => {
     }    
   }
 
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem} : item))
+  }
+
   // Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -50,10 +55,11 @@ export const FeedbackProvider = ({ children }) => {
 
   return <FeedbackContext.Provider value={{
     feedback,
+    feedbackEdit,
     deleteFeedback,
     addFeedback,
     editFeedback,
-    feedbackEdit,
+    updateFeedback,
   }}>
     {children}
   </FeedbackContext.Provider>
